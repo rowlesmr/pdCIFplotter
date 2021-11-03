add6d90b43f1dd2b45e3a99ffc31a94ae4309d60
rowlesmr <rowlesmr@gmail.com>
2021-11-03 13:50:04 +0800

    setting up so I can do pyz generation

---
+++ src/pdCIFplotter/__main__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+SPDX-License-Identifier:  GNU Lesser General Public License v3.0 or later
+
+Copyright © 2021 Matthew Rowles
+
+"""
+
+
+"""
+
+This file is here to make the pyz file work nicely.
+
+"""
+
+import gui_simple_2
+
+gui_simple_2.gui()
