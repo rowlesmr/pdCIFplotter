112c1c4d8cd580c5210fe2699fddfa1b376a2eb7
rowlesmr <rowlesmr@gmail.com>
2021-11-11 21:45:58 +0800

    Bump version: 0.0.3 → 0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'pdCIFplotter'
 year = '2021'
 author = 'Matthew Rowles'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.3'
+version = release = '0.0.4'
 
 pygments_style = 'trac'
 templates_path = ['.']
