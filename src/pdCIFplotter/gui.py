759f0d7ed6430c426b2154c4d00aa2161193c778
rowlesmr <rowlesmr@gmail.com>
2021-12-20 16:47:42 +0800

    Update gui.py

--- src/pdCIFplotter/gui.py
+++ src/pdCIFplotter/gui.py
@@ -28,8 +28,8 @@ sg.theme(MY_THEMES[THEME_NUMBER])
 
 print("somestuff")
 print(f"{mpl.__version__=}")
-# if mpl.__version__ <= "3.4.3":  # matplotlib bug workaround: https://github.com/matplotlib/matplotlib/issues/21875
-#     sg.set_options(dpi_awareness=True)
+if mpl.__version__ <= "3.4.3":  # matplotlib bug workaround: https://github.com/matplotlib/matplotlib/issues/21875
+    sg.set_options(dpi_awareness=True)
 
 # global parameters
 action_column_width = 30
