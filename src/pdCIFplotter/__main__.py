14e1bbd2a9aecd188c8e249ace92e95bb056990e
rowlesmr <rowlesmr@gmail.com>
2022-01-04 23:14:15 +0800

    fix typo

--- src/pdCIFplotter/__main__.py
+++ src/pdCIFplotter/__main__.py
@@ -1,5 +1,5 @@
 """
-Entrypoint module, in case you use `python -mciftostr`.
+Entrypoint module, in case you use `python -m pdcifplotter`.
 
 
 Why does this file exist, and why __main__? For more info, read:
