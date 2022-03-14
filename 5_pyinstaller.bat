79575d5ba8268b0e1629c96f28ed7bc961eebf0d
rowlesmr <rowlesmr@gmail.com>
2022-03-14 21:16:21 +0800

    Update 5_pyinstaller.bat

--- 5_pyinstaller.bat
+++ 5_pyinstaller.bat
@@ -1,5 +1,7 @@
 REM If it fails due to matplotlib, look at https://stackoverflow.com/a/67922414/36061
+REM --noupx see https://stackoverflow.com/a/28415085/36061
 cd pyinst
-pyinstaller --name "pdCIFplotter" --onefile ..\src\pdCIFplotter\__main__.py --icon ..\src\pdCIFplotter\icon.ico
+pyinstaller --name "pdCIFplotter" --onefile --noupx ..\src\pdCIFplotter\__main__.py --icon ..\src\pdCIFplotter\icon.ico
+REM pyinstaller --name "pdCIFplotter" --onedir --noupx ..\src\pdCIFplotter\__main__.py --icon ..\src\pdCIFplotter\icon.ico
 cd ..
 pause
